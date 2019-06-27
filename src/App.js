6af7b33adc70ec127da38cc9666b909e5c229ae8
rodrigoalmeidaas <rodrigo.almeida@agronow.com.br>
2019-06-26 21:19:49 -0300

    Ongoing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
+import { Paragraph }  from './paragraph';
 import './App.css';
 
 class App extends Component {
 
-  render() {
-    return (
-      <div className="App" style={{ paddingTop: '10px' }}>
-        <input type='text' />
-        <button>
-          Click me!
-        </button>
-        <h1>texto</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: 'Fiap',
+            data: '2019-06-26',
+            number: {
+                prop: 200
+            }
+        };
+
+        this.updateState = this.updateState.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    updateState() {
+        this.setState({name: 'Rodrigo'});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({name: event.target.value})
+    }
+
+    render() {
+        return (
+            <form className="App" style={{ paddingTop: '10px' }}>
+                <input type='text' value={this.state.name} onChange={this.handleChange}/>
+
+                <button type='submit' onSubmit={() => this.handleSubmit()}>Submit me!</button>
+                <button onClick={this.updateState}>Click me!</button>
+
+                <h1>{ this.state.data }</h1>
+                <Paragraph text={this.state.number.prop}/>
+                <Paragraph text={this.state.name}/>
+            </form>
+        )
+    }
+
 }
 export default App;
---
+++ src/paragraph.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const Paragraph = (props) => (<p>{ props.text }</p>);
+
